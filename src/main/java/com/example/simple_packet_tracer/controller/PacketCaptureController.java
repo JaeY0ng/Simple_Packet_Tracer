9894de4c747c569ab13658a58af827c3adf420de
Angelo <jyhhmj38@gmail.com>
2025-04-29 18:49:23 +0900

    PC Controller V2

--- src/main/java/com/example/simple_packet_tracer/controller/PacketCaptureController.java
+++ src/main/java/com/example/simple_packet_tracer/controller/PacketCaptureController.java
@@ -2,9 +2,16 @@ package com.example.simple_packet_tracer.controller;
 
 
 import com.example.simple_packet_tracer.service.PacketCaptureService;
+import org.pcap4j.core.NotOpenException;
+import org.pcap4j.core.PcapNativeException;
+import org.pcap4j.core.PcapNetworkInterface;
 import org.pcap4j.packet.Packet;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class PacketCaptureController {
@@ -16,14 +23,19 @@ public class PacketCaptureController {
     }
 
     @GetMapping("/interface")
-    public String listInterfaces() throws Exception{
-        packetCaptureService.listNetworkInterFace();
-        return "네트워크 인터페이스 조회 완료";
+    public List<String> listInterfaces() throws Exception{
+        List<PcapNetworkInterface> interfaces = packetCaptureService.listNetworkInterfaces();
+        return interfaces.stream()
+                .map(nif -> nif.getName() + " : " + nif.getDescription())
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/capture")
-    public String capturePackets() throws Exception{
-        packetCaptureService.capturePackets();
+    public String capturePackets(
+            @RequestParam String interfaceName,
+            @RequestParam(required = false) String bpfFilter
+    ) throws PcapNativeException, NotOpenException {
+        packetCaptureService.capturePackets(interfaceName, bpfFilter);
         return "패킷 캡처 완료";
     }
 }
